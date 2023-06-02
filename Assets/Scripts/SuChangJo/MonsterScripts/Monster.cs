42e5aea1e4dd8947c98122eb1902ad4913f807ac
suchangcho <tnckddl04@gmail.com>
2023-06-03 03:12:03 +0900

    0603_ScrollScripts

---
+++ Assets/Scripts/SuChangJo/MonsterScripts/Monster.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monster
+{
+    public int level = 1;
+    public int maxHp; //최대 체력
+    public int CurHP; //현재 체력
+    public int attack; //공격력
+    public int attackSpeed; //공격속도
+    public int speed; //스피드
+}
---
+++ Assets/Scripts/SuChangJo/MonsterScripts/MonsterBase.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Monster", menuName = "Monster/Create new Monster")] //파일이름은 몬스터이고 스크립트 만드는 화면에서 생성가능
+public class MonsterBase : ScriptableObject
+{
+    [SerializeField] string name;          //이름 선언 
+    //[SerializeField]:외부 스크립트에서 수정을 못하게 할려고 사용
+
+    [TextArea]
+    [SerializeField] string description; //캐릭터 설명
+
+    [SerializeField] Sprite monster; //몬스터 사진
+
+    public int level = 1;
+    public int maxHp; //최대 체력
+    public int CurHP; //현재 체력
+    public int attack; //공격력
+    public int attackSpeed; //공격속도
+    public int speed; //스피드
+
+}
